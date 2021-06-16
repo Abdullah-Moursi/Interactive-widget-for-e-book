0851b38f380a2a61381616cbd2487971b1a1ca3b
Abdullah-Moursi <moursiabdullah@gmail.com>
2021-06-16 10:58:35 +0200

    edit2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Fade from "react-reveal/Fade";
-import {Modal, ModalHelp} from "./Modal";
+import { Modal, ModalHelp } from "./Modal";
 import "./App.scss";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
@@ -11,9 +11,8 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import HashLoader from "react-spinners/HashLoader";
-import correct from '../src/correct.mp3';
-import incorrect from '../src/incorrect.mp3';
-
+import correct from "../src/correct.mp3";
+import incorrect from "../src/incorrect.mp3";
 
 const App = () => {
   const [curChar, setCurChar] = useState("");
@@ -26,45 +25,74 @@ const App = () => {
   const [pale, setPale] = useState(false);
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     setTimeout(() => {
-      setLoading(false)
+      setLoading(false);
     }, 1000);
-  }, [])
+  }, []);
 
   library.add(fab, faCheckSquare, faTimes, faTimesCircle);
 
-
   const playCorrect = () => {
     new Audio(correct).play();
-  }
+  };
 
   const playIncorrect = () => {
     new Audio(incorrect).play();
-  }
+  };
 
   const settingCharTap = () => {
     curChar === "a"
-      ? (function() {setTextTap("a"); playCorrect(); setCurChar(' ') })()
+      ? (function () {
+          setTextTap("a");
+          playCorrect();
+          setCurChar(" ");
+        })()
       : curChar === "p"
-      ? (function() {setTextTap("  p "); playIncorrect(); setTimeout(() => {setTextTap(' __ ')}, 500)})()
+      ? (function () {
+          setTextTap("  p ");
+          playIncorrect();
+          setTimeout(() => {
+            setTextTap(" __ ");
+          }, 500);
+        })()
       : setTextTap(" __ ");
   };
 
   const settingCharMap = () => {
     curChar === "a"
-      ? (function() {setTextMap("a"); playCorrect(); setCurChar('')})()
+      ? (function () {
+          setTextMap("a");
+          playCorrect();
+          setCurChar("");
+        })()
       : curChar === "p"
-      ? (function() {setTextMap("  p "); playIncorrect();  setTimeout(() => {setTextMap(' __ ')}, 500)})()
+      ? (function () {
+          setTextMap("  p ");
+          playIncorrect();
+          setTimeout(() => {
+            setTextMap(" __ ");
+          }, 500);
+        })()
       : setTextMap(" __ ");
   };
 
   const settingCharPat = () => {
     curChar === "p"
-    ? (function() {setTextPat("p"); playCorrect(); setCurChar('')})()
-    : curChar === "a"
-    ? (function() {setTextPat("  a "); playIncorrect();  setTimeout(() => {setTextPat(' __ ')}, 500)})()
-    : setTextPat(" __ ");
+      ? (function () {
+          setTextPat("p");
+          playCorrect();
+          setCurChar("");
+        })()
+      : curChar === "a"
+      ? (function () {
+          setTextPat("  a ");
+          playIncorrect();
+          setTimeout(() => {
+            setTextPat(" __ ");
+          }, 500);
+        })()
+      : setTextPat(" __ ");
   };
 
   const refresh = () => {
@@ -72,6 +100,7 @@ const App = () => {
     setTextPat(" __ ");
     setTextTap(" __ ");
     setCurChar("");
+    setPale(false);
   };
 
   const reveal = () => {
@@ -84,196 +113,232 @@ const App = () => {
 
   return (
     <div className="App">
-      {
-        loading ? <div className='preload'> <HashLoader color={'#0FA0C5'} loading={loading} size={130} /> </div>
-        :
+      {loading ? (
+        <div className="preload">
+          {" "}
+          <HashLoader color={"#0FA0C5"} loading={loading} size={130} />{" "}
+        </div>
+      ) : (
         <Fade right cascade>
-        <header>
-          <div className="header">
-            <div className="title">
-              <h1>Complete the words.</h1>
-            </div>
-            <div className="up-imgs">
-              <div className="resource">
-                <img
-                  onClick={() => setIsOpen(true)}
-                  src="https://educationalrc.org/tasks/3/assets/images/resourse_icon.png"
-                  className="img-responsive"
-                  alt="resource"
-                ></img>
-                <Modal dummy={'portal'} open={isOpen} onClose={() => setIsOpen(false)}>
+          <header>
+            <div className="header">
+              <div className="title">
+                <h1>Complete the words.</h1>
+              </div>
+              <div className="up-imgs">
+                <div className="resource">
                   <img
-                    className="portalImg"
-                    alt="dummy"
-                    src="https://educationalrc.org/tasks/3/assets/images/dummy.jpg"
-                  />
-                </Modal>
-              </div>
-              <div className="help">
-                <img
-                  onClick={() => setIsOpenHelp(true)}
-                  src="https://educationalrc.org/tasks/3/assets/images/help_icon.png"
-                  className="img-responsive"
-                  alt="help"
-                ></img>
+                    onClick={() => setIsOpen(true)}
+                    src="https://educationalrc.org/tasks/3/assets/images/resourse_icon.png"
+                    className="img-responsive"
+                    alt="resource"
+                  ></img>
+                  <Modal
+                    dummy={"portal"}
+                    open={isOpen}
+                    onClose={() => setIsOpen(false)}
+                  >
+                    <img
+                      className="portalImg"
+                      alt="dummy"
+                      src="https://educationalrc.org/tasks/3/assets/images/dummy.jpg"
+                    />
+                  </Modal>
+                </div>
+                <div className="help">
+                  <img
+                    onClick={() => setIsOpenHelp(true)}
+                    src="https://educationalrc.org/tasks/3/assets/images/help_icon.png"
+                    className="img-responsive"
+                    alt="help"
+                  ></img>
 
-                <ModalHelp
-                help={'portalHelp'}
-                  open={isOpenHelp}
-                  onClose={() => setIsOpenHelp(false)}
+                  <ModalHelp
+                    help={"portalHelp"}
+                    open={isOpenHelp}
+                    onClose={() => setIsOpenHelp(false)}
+                  >
+                    <h1>Help content goes here</h1>
+                  </ModalHelp>
+                </div>
+              </div>
+            </div>
+          </header>
+          <div className="back">
+            <div className="container">
+              <div className="question-container">
+                <button
+                  onClick={() => pale === false && setCurChar("a")}
+                  className={`${
+                    curChar === "a" ? "focus" : pale ? "pale" : "question"
+                  }`}
                 >
-                  <h1>Help content goes here</h1>
-                </ModalHelp>
+                  a
+                </button>
+                <button
+                  onClick={() => pale === false && setCurChar("p")}
+                  className={`${
+                    curChar === "p" ? "focus" : pale ? "pale" : "question"
+                  }`}
+                >
+                  p
+                </button>
               </div>
-            </div>
-          </div>
-        </header>
-        <div className="back">
-        <div className="container">
-          <div className="question-container">
-            <button
-              onClick={() => pale === false && setCurChar('a')}
-              className={`${curChar === "a" ? "focus" : pale ? "pale" : "question"}`}
-            >
-              a
-            </button>
-            <button
-              onClick={() => pale === false && setCurChar('p')}
-              className={`${curChar === "p" ? "focus" : pale ? "pale" : "question"}`}
-            >
-              p
-            </button>
-          </div>
-          <div className="option-container">
-            <div className="line">
-              <div className="opt1">
-                <div className="opt-text">
-                  <span onClick={() => settingCharTap()} className="optClick">
-                    {" "}
-                    {textTap === "a" ? (
-                      <FontAwesomeIcon className="right" icon="check-square" />
-                    ) : textTap === "  p " ? (
-                      <FontAwesomeIcon className="false" icon="times-circle" />
-                    ) : (
-                      ""
-                    )}
-                    1 t
-                    {
+              <div className="option-container">
+                <div className="line">
+                  <div className="opt1">
+                    <div className="opt-text">
                       <span
-                        className={`${
-                          textTap === " __ "
-                            ? ""
-                            : textTap === "  p "
-                            ? "not-answer"
-                            : "answer"
-                        }`}
+                        onClick={() => settingCharTap()}
+                        className="optClick"
                       >
-                        {textTap}
-                      </span>
-                    }
-                    p{" "}
-                  </span>{" "}
-                </div>
-                <img
-                  alt="tap"
-                  src="https://educationalrc.org/tasks/3/assets/images/p19e24/p19ex24q1_1.png"
-                />
-              </div>
+                        {" "}
+                        {textTap === "a" ? (
+                          <FontAwesomeIcon
+                            className="right"
+                            icon="check-square"
+                          />
+                        ) : textTap === "  p " ? (
+                          <FontAwesomeIcon
+                            className="false"
+                            icon="times-circle"
+                          />
+                        ) : (
+                          ""
+                        )}
+                        1 t
+                        {
+                          <span
+                            className={`${
+                              textTap === " __ "
+                                ? ""
+                                : textTap === "  p "
+                                ? "not-answer"
+                                : "answer"
+                            }`}
+                          >
+                            {textTap}
+                          </span>
+                        }
+                        p{" "}
+                      </span>{" "}
+                    </div>
+                    <img
+                      alt="tap"
+                      src="https://educationalrc.org/tasks/3/assets/images/p19e24/p19ex24q1_1.png"
+                    />
+                  </div>
 
-              <div className="opt3">
-                <div className="opt-text">
-                  <span onClick={() => settingCharMap()} className="optClick">
-                    {textMap === "a" ? (
-                      <FontAwesomeIcon className="right" icon="check-square" />
-                    ) : textMap === "  p " ? (
-                      <FontAwesomeIcon className="false" icon="times-circle" />
-                    ) : (
-                      ""
-                    )}
-                    3 m
-                    {
+                  <div className="opt3">
+                    <div className="opt-text">
                       <span
-                        className={`${
-                          textMap === " __ "
-                            ? ""
-                            : textMap === "  p "
-                            ? "not-answer"
-                            : "answer"
-                        }`}
+                        onClick={() => settingCharMap()}
+                        className="optClick"
                       >
-                        {textMap}
-                      </span>
-                    }
-                    p
-                  </span>{" "}
+                        {textMap === "a" ? (
+                          <FontAwesomeIcon
+                            className="right"
+                            icon="check-square"
+                          />
+                        ) : textMap === "  p " ? (
+                          <FontAwesomeIcon
+                            className="false"
+                            icon="times-circle"
+                          />
+                        ) : (
+                          ""
+                        )}
+                        3 m
+                        {
+                          <span
+                            className={`${
+                              textMap === " __ "
+                                ? ""
+                                : textMap === "  p "
+                                ? "not-answer"
+                                : "answer"
+                            }`}
+                          >
+                            {textMap}
+                          </span>
+                        }
+                        p
+                      </span>{" "}
+                    </div>
+                    <img
+                      alt="map"
+                      src="https://educationalrc.org/tasks/3/assets/images/p19e24/p19ex24q1_3.png"
+                    />
+                  </div>
                 </div>
-                <img
-                  alt="map"
-                  src="https://educationalrc.org/tasks/3/assets/images/p19e24/p19ex24q1_3.png"
-                />
-              </div>
-            </div>
 
-            <div className="line">
-              <div className="opt2">
-                <div className="opt-text">
-                  <span onClick={() => settingCharPat()} className="optClick">
-                    {textPat === "p" ? (
-                      <FontAwesomeIcon className="right" icon="check-square" />
-                    ) : textPat === "  a " ? (
-                      <FontAwesomeIcon className="false" icon="times-circle" />
-                    ) : (
-                      ""
-                    )}
-                    2{" "}
-                    {
+                <div className="line">
+                  <div className="opt2">
+                    <div className="opt-text">
                       <span
-                        className={`${
-                          textPat === " __ "
-                            ? ""
-                            : textPat === "  a "
-                            ? "not-answer"
-                            : "answer"
-                        }`}
+                        onClick={() => settingCharPat()}
+                        className="optClick"
                       >
-                        {textPat}
-                      </span>
-                    }
-                    at
-                  </span>{" "}
+                        {textPat === "p" ? (
+                          <FontAwesomeIcon
+                            className="right"
+                            icon="check-square"
+                          />
+                        ) : textPat === "  a " ? (
+                          <FontAwesomeIcon
+                            className="false"
+                            icon="times-circle"
+                          />
+                        ) : (
+                          ""
+                        )}
+                        2{" "}
+                        {
+                          <span
+                            className={`${
+                              textPat === " __ "
+                                ? ""
+                                : textPat === "  a "
+                                ? "not-answer"
+                                : "answer"
+                            }`}
+                          >
+                            {textPat}
+                          </span>
+                        }
+                        at
+                      </span>{" "}
+                    </div>
+                    <img
+                      alt="pat"
+                      src="https://educationalrc.org/tasks/3/assets/images/p19e24/p19ex24q1_2.png"
+                    />
+                  </div>
                 </div>
-                <img
-                  alt="pat"
-                  src="https://educationalrc.org/tasks/3/assets/images/p19e24/p19ex24q1_2.png"
-                />
               </div>
             </div>
-          </div>
-        </div>
-        
-        <footer>
-          <div className="footer">
-            <div className="refresh">
-              <img
-                onClick={() => refresh()}
-                alt="refresh"
-                src="https://educationalrc.org/tasks/3/assets/images/replay_icon.png"
-              />
-            </div>
-            <div className="reveal">
-              <img
-                onClick={() => reveal()}
-                alt="reveal"
-                src="https://educationalrc.org/tasks/3/assets/images/showAns_icon.png"
-              />
-            </div>
-          </div>
-        </footer>
-        </div>
 
-      </Fade>
-      }
-    
+            <footer>
+              <div className="footer">
+                <div className="refresh">
+                  <img
+                    onClick={() => refresh()}
+                    alt="refresh"
+                    src="https://educationalrc.org/tasks/3/assets/images/replay_icon.png"
+                  />
+                </div>
+                <div className="reveal">
+                  <img
+                    onClick={() => reveal()}
+                    alt="reveal"
+                    src="https://educationalrc.org/tasks/3/assets/images/showAns_icon.png"
+                  />
+                </div>
+              </div>
+            </footer>
+          </div>
+        </Fade>
+      )}
     </div>
   );
 };
